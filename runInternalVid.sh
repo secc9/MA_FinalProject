d99c3c694ac669c2c483dbfdc7f9eb33c5c2988f
secc9 <noisebox909@gmail.com>
2024-07-25 18:18:24 +0100

    updating.sh

--- runInternalVid.sh
+++ runInternalVid.sh
@@ -4,4 +4,14 @@
   #  ffplay -probesize 500000 -fflags nobuffer -flags low_delay -framedrop -protocol_whitelist "file,udp,rtp" -vf "pixelize=w=20:h=20:mode=max" udp://127.0.0.1:5000
 
 
-  ffplay -probesize 500000 -fflags nobuffer -flags low_delay -framedrop -protocol_whitelist "file,udp,rtp" -vf "pixelize=w=20:h=20:mode=max" udp://127.0.0.1:5000
+ffplay \
+     udp://127.0.0.1:5000 \
+    -probesize 5000000 \
+    -fflags nobuffer \
+    -flags low_delay \
+    -framedrop \
+    -protocol_whitelist "file,udp,rtp" \
+    -vf "libplacebo=w=iw*2:h=ih*2:extra_opts='upscaler=custom\:upscaler_preset=ewa_lanczos\:upscaler_blur=0.9812505644269356'"
+
+    # -vf "shuffleframes= 0 2 1,stereo3d=in=sbs2r:out=chl,amplify=radius=10:factor=8:threshold=200,blockdetect=period_min=3:period_max=10:planes=1" 
+
